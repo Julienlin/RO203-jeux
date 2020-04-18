0c2053444e264272cee5cc7f7b4db993e9d7c8fe
Julien Lin <lin.julien@ensta-paris.fr>
2020-04-18 21:25:07 +0200

    test new things

--- jeu1/src/main.jl
+++ jeu1/src/main.jl
@@ -2,9 +2,10 @@ include("io.jl")
 include("resolution.jl")
 
 function main()
-    instance::UndeadInstance = readInputFile("./src/ex_instance.txt")
+    instance::UndeadInstance = readInputFile("ex_instance_full_zombies_one_mirror.txt")
     displayGrid(instance)
     println(cplexSolve(instance))
+    displaySolution(instance)
 
 end
 
