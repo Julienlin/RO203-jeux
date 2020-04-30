b20c131a9458164f75868b61f8ec8ff01ed7c772
Julien Lin <lin.julien@ensta-paris.fr>
2020-04-30 20:59:13 +0200

    udpates

--- jeu1/src/instance.jl
+++ jeu1/src/instance.jl
@@ -37,7 +37,7 @@ function UndeadInstance(inst::UndeadInstance)
     V = inst.V
     C = copy(inst.C)
     Y = copy(inst.Y)
-    return UndeadInstance(N,X,Z,G,V,C,Y)
+    return UndeadInstance(N, X, Z, G, V, C, Y)
 end
 
 struct HeuristicInstance
@@ -49,7 +49,7 @@ struct HeuristicInstance
     V::Int64
     C::Vector{Vector{Vector{Int64}}}
     Y::Array{Int64}
-    P:: Array{Vector{Int64},2} #Matrice des possibilites
+    P::Array{Vector{Int64},2} # Matrice des possibilites
 end
 
 function HeuristicInstance(inst::UndeadInstance)
@@ -69,13 +69,13 @@ function HeuristicInstance(inst::UndeadInstance)
         str_rep *= string(i)
     end
     P = get_possibilities(inst)
-    return HeuristicInstance(str_rep, N,X,Z,G,V,C,Y,P)
+    return HeuristicInstance(str_rep, N, X, Z, G, V, C, Y, P)
 end
 
 
 
 function get_unfilled_boxes(inst::HeuristicInstance)
-    res = Vector{Vector{Int64}}(undef,0)
+    res = Vector{Vector{Int64}}(undef, 0)
     N = inst.N
     for i in 1:N[1]
         for j in 1:N[2]
@@ -87,7 +87,7 @@ function get_unfilled_boxes(inst::HeuristicInstance)
     return res
 end
 
-function is_contained_get_value( path::Vector{Vector{Int64}}, i::Int64, j::Int64 )
+function is_contained_get_value(path::Vector{Vector{Int64}}, i::Int64, j::Int64)
     for el in path
         if el[1] == i && el[2] == j
             return (true, el[3])
@@ -98,18 +98,20 @@ end
 
 
 function get_paths_in_stakes(inst, i::Int64, j::Int64)
-    paths = Vector{Vector{Int64}}(undef,0)
+    paths = Vector{Vector{Int64}}(undef, 0)
     # Get indices of paths that contained the box and the value associated with
-    for k in 1:size(inst.C,1)
-        is_contained, value = is_contained_get_value(inst.C[k],i,j)
+    for k in 1:size(inst.C, 1)
+        is_contained, value = is_contained_get_value(inst.C[k], i, j)
         if is_contained
-            push!(paths,[k, value, inst.Y[k]])
+            push!(paths, [k, value, inst.Y[k]])
         end
     end
     return paths
 end
 
-
+"""
+Function that create the matrix of possibilites for a given instance.
+"""
 function get_possibilities(inst)
     N = inst.N
     P = Array{Vector{Int64},2}(undef, N[1], N[2])
@@ -119,10 +121,10 @@ function get_possibilities(inst)
                 paths_in_stakes = get_paths_in_stakes(inst, i, j)
 
                 # fecthing conditions
-                possibility = Vector{Int64}(undef,0)
+                possibility = Vector{Int64}(undef, 0)
                 # Testing if we can add a ghost
                 if inst.G > 0
-                    is_valid = reduce(&, map( x -> x[2] != 0 || x[3] - 1 >= 0 , paths_in_stakes ))
+                    is_valid = reduce(&, map(x->x[2] != 0 || x[3] - 1 >= 0, paths_in_stakes))
                     if is_valid
                         push!(possibility, 1)
                     end
@@ -130,7 +132,7 @@ function get_possibilities(inst)
 
                 # Testing if we can add a vampire
                 if inst.V > 0
-                    is_valid = reduce(&, map( x -> x[2] != 1 || x[3] - 1 >= 0 , paths_in_stakes ))
+                    is_valid = reduce(&, map(x->x[2] != 1 || x[3] - 1 >= 0, paths_in_stakes))
                     if is_valid
                         push!(possibility, 3)
                     end
@@ -138,7 +140,7 @@ function get_possibilities(inst)
 
                 # Testing if we can add a zombie
                 if inst.Z > 0
-                    is_valid = reduce(&,map( x -> x[3]-1 >= 0, paths_in_stakes ))
+                    is_valid = reduce(&, map(x->x[3] - 1 >= 0, paths_in_stakes))
                     if is_valid
                         push!(possibility, 2)
                     end
@@ -161,28 +163,19 @@ end
 Funtion that sort the vector of cells by the ascending number of possibilities.
 """
 function sort_by_possibilities(inst::HeuristicInstance, cells::Vector{Vector{Int64}})
-    return sort(cells, by=x ->
-                length(get_paths_in_stakes(inst,x[1],x[2])))
+    return sort(cells, by = x->length(get_paths_in_stakes(inst, x[1], x[2])))
 
     # return sort(cells, by=x -> length(inst.P[x[1],x[2]]) *
     #             (1 - length(get_paths_in_stakes(inst,x[1],x[2]))/length(inst.Y) ))
 end
 
-function sort_by_entropy(inst, cells::Vector{Vector{Int64}})
-    return sort(cells, by= x-> entropy(inst, x))
-end
-
-function entropy(inst, x::Vector{Int64})
-    probZ = inst.Z / prod(inst.N)
-    probG = inst.G / prod(inst.N)
-    probV = inst.V / prod(inst.N)
-    res = probZ + probG + probV
-   
+function sort_by_paths_length(inst::HeuristicInstance, cells::Vector{Vector{Int64}})
+    # return sort(cells, by = x -> min(map( x -> )))
 end
 
 
 function create_modified(inst::HeuristicInstance, i::Int64, j::Int64, v::Int64)
-    if v >0 && v < 4 && inst.X[i,j] == 0
+    if v > 0 && v < 4 && inst.X[i,j] == 0
         N = copy(inst.N)
         X = copy(inst.X)
         G = inst.G
@@ -203,7 +196,7 @@ function create_modified(inst::HeuristicInstance, i::Int64, j::Int64, v::Int64)
         end
 
         # Update Y
-        paths_in_stakes = get_paths_in_stakes(inst, i,j)
+        paths_in_stakes = get_paths_in_stakes(inst, i, j)
 
         for el in paths_in_stakes
             if v == 1
@@ -220,7 +213,7 @@ function create_modified(inst::HeuristicInstance, i::Int64, j::Int64, v::Int64)
         end
 
         # We create a copy of the HeuristicInstance
-        new_inst = HeuristicInstance(UndeadInstance(N,X,Z,G,V,C,Y))
+        new_inst = HeuristicInstance(UndeadInstance(N, X, Z, G, V, C, Y))
         return new_inst
     end
     return nothing
@@ -251,7 +244,7 @@ function is_valid(inst::HeuristicInstance)
     return true
 end
 
-function is_finished(inst:: HeuristicInstance)
+function is_finished(inst::HeuristicInstance)
     # Test whether there is a type of monster that is over use
     if inst.Z != 0 || inst.G != 0 || inst.V != 0
         return false
